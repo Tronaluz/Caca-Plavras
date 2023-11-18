4ea0cfc5722e60365aa0490f60cd10c83e7f2885
Tronaluz <lucas_tron@hotmail.com>
2023-11-18 20:32:13 -0300

    first

--- Test.java
+++ Test.java
@@ -1,2 +1,7 @@
 public class Test {
-}
+    test('espera que a função contaPalavras retorne 5', () => {
+ const texto = 'Este é um exemplo de texto';
+ const palavras = contaPalavras(texto);
+        expect(palavras).toBe(5);
+    });
+
